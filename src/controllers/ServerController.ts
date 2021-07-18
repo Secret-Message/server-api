2820cdd5c2aca4687d94cf91ee70f18d69041a3d
Frankoslaw <franopusz2006@gmail.com>
2021-07-18 12:37:26 +0200

    Implemented server controller

--- src/controllers/ServerController.ts
+++ src/controllers/ServerController.ts
@@ -13,20 +13,104 @@ export class ServerController {
     private member_service: MemberService = new MemberService();
 
     public createServer(req: CustomRequest, res: Response ) {
-        notWorkingYet(null, res)
+        if( req.body.name ) {
+                const server_params: IServer = {
+                    name: req.body.name,
+                    iconUrl: req.body.iconUrl,
+                    dm: false
+                }
+
+                this.server_service.create(server_params, (err: any, server_data: IServer) => {
+                    if (err) {
+                        mongoError(err, res);
+                    } else {
+                        const member_params: IMember = {
+                            server: server_data._id,
+                            isOwner: true,
+                            user: req.decoded.userId
+                        }
+
+                        this.member_service.create(member_params, (err: any, member_data: IMember) => {
+                            if (err) {
+                                mongoError(err, res);
+                            } else {
+                                successResponse('Create server successfull', { 
+                                    server: server_data,
+                                    member: member_data
+                                }, res);
+                            }
+                        })
+                    }
+                });
+        } else {
+           insufficientParameters(res);
+        }
     }
 
     public getServer(req: CustomRequest, res: Response ) {
-        notWorkingYet(null, res)
+        if( req.params.serverId ) {
+            this.server_service.getOne({
+                _id: req.params.serverId
+            }, (err: any, server_data: IServer) => {
+                if (err) {
+                    mongoError(err, res);
+                } else {
+                    if( server_data ){
+                        successResponse('Server found', server_data, res);
+                    }else{
+                        successResponse('Server don\'t exist', server_data, res);
+                    }
+                }
+            });
+        } else {
+            insufficientParameters(res);
+        }
     }
 
     public updateServer(req: CustomRequest, res: Response ) {
-        notWorkingYet(null, res)
+        if ( req.params.serverId &&
+            req.body.name ||
+            req.body.iconUrl ) {
+           this.server_service.getOne({ 
+               _id: req.params.serverId
+           }, (err: any, server_data: IServer) => {
+               if (err) {
+                   mongoError(err, res);
+               }else {
+                   const server_params: IServer = {
+                       _id: req.params.serverId,
+                       name: req.body.name ? req.body.name : server_data.name,
+                       iconUrl: req.body.iconUrl ? req.body.iconUrl : server_data.iconUrl,
+                       dm: server_data.dm
+                   };
+
+                   this.server_service.update(server_params, (err: any) => {
+                       if (err) {
+                           mongoError(err, res);
+                       } else {
+                           successResponse('Update server successfull', null, res);
+                       }
+                   });
+               }
+           })
+       } else {
+           insufficientParameters(res);
+       }
     }
 
     public deleteServer(req: CustomRequest, res: Response ) {
-        notWorkingYet(null, res)
+        if ( req.params.serverId ){
+            this.server_service.delete(req.params.serverId, 
+                (err: any, server_data: IServer) => {
+                if (err) {
+                    mongoError(err, res);
+                }else {
+                    successResponse('Server deleted', server_data, res);
+                }
+            })
+        } else {
+            insufficientParameters(res);
+        }
     }
-
 
 }
